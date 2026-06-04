bc0f94282b46a4e3fef51785ccaed8bf4a7a484d
Hans Müller <iraqihu1994@gmail.com>
2026-06-04 17:37:07 +0200

    Implemented first styling of the the header and the atf section.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Atf } from './components/atf/atf';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Atf],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('poll-app');
