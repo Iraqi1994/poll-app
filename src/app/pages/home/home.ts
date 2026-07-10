8a0fa5cb17e301dd9a692d375d16b847c1f5ac3d
Hans Müller <iraqihu1994@gmail.com>
2026-07-10 16:48:18 +0200

    Implemented the first version of the formGroup for new surveys.

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { Header } from '../../components/header/header';
 import { Atf } from '../../components/atf/atf';
 import { YourSurveys } from '../../components/your-surveys/your-surveys';
 import { ActiveSurveys } from '../../components/active-surveys/active-surveys';
 
 @Component({
   selector: 'app-home',
-  imports: [Atf, YourSurveys, ActiveSurveys],
+  imports: [Header, Atf, YourSurveys, ActiveSurveys],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
--- src/app/pages/new-survey-form/new-survey-form.ts
+++ src/app/pages/new-survey-form/new-survey-form.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-survey-form',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './new-survey-form.html',
   styleUrl: './new-survey-form.scss',
 })
-export class NewSurveyForm {}
+export class NewSurveyForm {
+  surveyForm = new FormGroup({
+    name: new FormControl('', { validators: [Validators.required, Validators.minLength(3)] }),
+    description: new FormControl('', { validators: [Validators.maxLength(200)] }),
+    endDate: new FormControl(''),
+  });
+}
