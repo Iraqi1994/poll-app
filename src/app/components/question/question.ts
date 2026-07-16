359d02bf2ac097d43df9afca4233fe309f65c3f5
Hans Müller <iraqihu1994@gmail.com>
2026-07-16 17:28:46 +0200

    Implemented the first version of the new survey form. Also started with the styling of the new survey page.

--- src/app/components/question/question.ts
+++ src/app/components/question/question.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-question',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './question.html',
   styleUrl: './question.scss',
 })
 export class Question {
+  questionGroup = input.required<FormGroup>();
+  questionIndex = input<number>(1);
 
+  get answers(): FormArray {
+    return this.questionGroup().get('answers') as FormArray;
+  }
+
+  get answerControls(): FormControl[] {
+    return this.answers.controls as FormControl[];
+  }
+
+  getAnswerLabel(index: number): string {
+    return String.fromCharCode(65 + index);
+  }
+
+  addAnswer(): void {
+    this.answers.push(new FormControl(''));
+  }
 }
--- src/app/pages/new-survey-form/new-survey-form.ts
+++ src/app/pages/new-survey-form/new-survey-form.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Question } from '../../components/question/question';
 
 @Component({
   selector: 'app-new-survey-form',
-  imports: [RouterLink, ReactiveFormsModule],
+  imports: [RouterLink, ReactiveFormsModule, Question],
   templateUrl: './new-survey-form.html',
   styleUrl: './new-survey-form.scss',
 })
@@ -13,5 +14,26 @@ export class NewSurveyForm {
     name: new FormControl('', { validators: [Validators.required, Validators.minLength(3)] }),
     description: new FormControl('', { validators: [Validators.maxLength(200)] }),
     endDate: new FormControl(''),
+    questions: new FormArray([this.createQuestion(true)]),
   });
+
+  get questions(): FormArray {
+    return this.surveyForm.get('questions') as FormArray;
+  }
+
+  get questionGroups(): FormGroup[] {
+    return this.questions.controls as FormGroup[];
+  }
+
+  private createQuestion(required: boolean): FormGroup {
+    return new FormGroup({
+      text: new FormControl('', required ? [Validators.required] : []),
+      allowMultiple: new FormControl(false),
+      answers: new FormArray([new FormControl(''), new FormControl('')]),
+    });
+  }
+
+  addQuestion(): void {
+    this.questions.push(this.createQuestion(false));
+  }
 }
